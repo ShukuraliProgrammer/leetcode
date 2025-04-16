00c213034ba929a8ae622b01848d269c70823266
Shukurali Rezamonov <shukuralijob@gmail.com>
2025-04-16 10:17:04 +0500

    guess number problem

---
+++ Daily_Challenges/find_prime_number.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+        else:
+            continue
+    return True
+
+
+def get_prime(num):
+    primes = []
+    for i in range(num):
+        if is_prime(i):
+            primes.append(i)
+        else:
+            continue
+    return primes
+
+
+print(get_prime(10))
